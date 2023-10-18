6610a1187696dc1a731202231ed3545101a2d1ef
Hycarus <mirkomarasco08@gmail.com>
2023-10-18 14:50:06 +0200

    added width and height from js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ btn.addEventListener('click', function(){
     
     // ciclo for per stampare i quadratini
     for(let i = 0; i < numBox; i++){
-        let box = drawBox(i);
+        let box = drawBox(i, numBox);
         console.log(box);
         wrapper.append(box);
     }
@@ -16,14 +16,17 @@ btn.addEventListener('click', function(){
 })
 
 // disegno un quadrato
-function drawBox(indexBox){
+function drawBox(indexBox, numCell){
     const box = document.createElement('div');
     box.classList.add('box');
+    box.style.width = `calc(100% / ${Math.sqrt(numCell)})`;
+    box.style.height = box.style.width;
     box.innerHTML = indexBox;
     box.style.color = 'white';
     box.addEventListener('click', function(){
         box.classList.add('active');
         box.style.color = 'black';
+        console.log(indexBox)
     })
     return box;
 }

29267bf2cceb1782a2a070ac5bd65454e952e8dd
Hycarus <mirkomarasco08@gmail.com>
2023-10-18 14:10:58 +0200

    created function for draw box

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ const btn = document.querySelector('button');
 
 
 
+
+
+
+// disegno un quadrato
+function drawBox(){
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.addEventListener('click', function(){
+        box.classList.add('active');
+
+    })
+    return box;
+}
+

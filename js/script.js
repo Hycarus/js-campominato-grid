757acbe7975e5014079555999b03ff7968fc5a16
Hycarus <mirkomarasco08@gmail.com>
2023-10-18 14:11:35 +0200

    created event for btn for generate box

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 const btn = document.querySelector('button');
 
-
-
-
-
+btn.addEventListener('click', function(){
+    // generare tot (numBox) quadratini
+    
+    const numBox = 100;
+    const wrapper = document.getElementById('wrapper');
+    
+    // ciclo for per stampare i quadratini
+    for(let i = 0; i < numBox; i++){
+        let box = drawBox();
+        console.log(box);
+        wrapper.append(box);
+    }
+    
+})
 
 // disegno un quadrato
 function drawBox(){
